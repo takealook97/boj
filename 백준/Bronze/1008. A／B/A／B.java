ac6d765ccf8eb0cdbae420b3d55611a8dcdc1ef5
takealook97 <118447769+takealook97@users.noreply.github.com>
2022-11-17 22:03:33 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double a,b;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextDouble();
+        b = scan.nextDouble();
+
+        System.out.println(a/b);
+    }
+}
