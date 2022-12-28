762908a4ae687568f155120fab5871aacf1f6524
takealook97 <118447769+takealook97@users.noreply.github.com>
2022-12-28 20:53:19 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
         int cnt = 0;
         while (N >= 5) {
             cnt += N / 5;
