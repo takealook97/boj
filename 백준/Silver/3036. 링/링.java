66dc6e1e5a40fb4247958f56b2efa76c796f0303
takealook97 <118447769+takealook97@users.noreply.github.com>
2022-12-28 19:42:10 +0900

    [Silver IV] Title: 링, Time: 260 ms, Memory: 18476 KB -BaekjoonHub

---
+++ 백준/Silver/3036. 링/링.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 1; i < N; i++) {
+            System.out.println(arr[0] / gcd(arr[0], arr[i]) + "/" + arr[i] / gcd(arr[0], arr[i]));
+        }
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
