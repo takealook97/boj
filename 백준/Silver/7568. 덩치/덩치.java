a13db9142502aefbefed53e6741807a958dfb7b7
takealook <takealook97@naver.com>
2023-01-21 19:10:26 +0900

    [Silver V] Title: 덩치, Time: 240 ms, Memory: 18396 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] arr = new int[N][2];
+        int[] result = new int[N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    result[i]++;
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            System.out.print(result[i] + 1 + " ");
+        }
+    }
+}
