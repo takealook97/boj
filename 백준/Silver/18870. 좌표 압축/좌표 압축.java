02af0f05508c15daf3d0701e7f63bdaf6a093ddb
takealook <takealook97@naver.com>
2023-01-30 23:42:53 +0900

    [Silver II] Title: 좌표 압축, Time: 1900 ms, Memory: 263128 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        int[] sortedArr = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sortedArr[i] = arr[i];
+        }
+        Arrays.sort(sortedArr);
+        int index = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            if (!map.containsKey(sortedArr[i])) {
+                map.put(sortedArr[i], index++);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(map.get(arr[i])).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
