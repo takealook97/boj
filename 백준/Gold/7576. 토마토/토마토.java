50981839750f74955eb8528e02b35c133d5d0f9a
takealook <takealook97@naver.com>
2023-02-15 00:13:57 +0900

    [Gold V] Title: 토마토, Time: 716 ms, Memory: 129312 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -13,7 +13,7 @@ public class Main {
         M = Integer.parseInt(st.nextToken());//가로
         N = Integer.parseInt(st.nextToken());//세로
         int[][] arr = new int[N][M];
-        HashSet<Location> list = new HashSet<>();
+        List<Location> list = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < M; j++) {
@@ -24,38 +24,37 @@ public class Main {
         int count = 0;
 
         while (true) {
-            HashSet<Location> temp = new HashSet<>();
+            List<Location> temp = new ArrayList<>();
             for (Location location : list) {
                 int i = location.x;
                 int j = location.y;
                 if (0 <= i - 1) {
                     if (arr[i - 1][j] == 0) {
+                        arr[i - 1][j] = 1;
                         temp.add(new Location(i - 1, j));
                     }
                 }
                 if (i + 1 <= N - 1) {
                     if (arr[i + 1][j] == 0) {
+                        arr[i + 1][j] = 1;
                         temp.add(new Location(i + 1, j));
                     }
                 }
                 if (0 <= j - 1) {
                     if (arr[i][j - 1] == 0) {
+                        arr[i][j - 1] = 1;
                         temp.add(new Location(i, j - 1));
                     }
                 }
                 if (j + 1 <= M - 1) {
                     if (arr[i][j + 1] == 0) {
+                        arr[i][j + 1] = 1;
                         temp.add(new Location(i, j + 1));
                     }
                 }
             }//맵에 한바퀴 다 돌고 바꿀 인덱스 넣어놓은 상태
 
             if (temp.isEmpty()) break; //바꿀게 없는 경우
-            else {//바꿀게 있을경우
-                for (Location temporary : temp) {
-                    arr[temporary.x][temporary.y] = 1;//인덱스값에 해당하는 수 모두 1로 변경
-                }
-            }
             count++;//카운트 증가
             list = temp;
 
@@ -72,16 +71,6 @@ public class Main {
         }
         return true;
     }
-
-    static void printArray(int[][] arr) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
 }
 
 class Location {
@@ -91,18 +80,5 @@ class Location {
     public Location(int x, int y) {
         this.x = x;
         this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if(obj instanceof Location) {
-            return this.x == ((Location) obj).x && this.y == ((Location) obj).y;
-        }
-        return super.equals(obj);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
     }
 }
