a3b8a0975d8bb27a609c789db9f8dba5d941fed9
takealook <takealook97@naver.com>
2023-03-14 22:51:21 +0900

    [Silver III] Title: N과 M (1), Time: 2152 ms, Memory: 64308 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] check;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new int[M];
+        check = new boolean[N];
+        dfs(0);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (!check[i]) {
+                check[i] = true;
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+                check[i] = false;
+            }
+        }
+
+    }
+}
