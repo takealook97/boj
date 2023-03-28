d573271fec13ce12438e151897f3c17dbfe724b8
takealook <takealook97@naver.com>
2023-03-28 23:23:36 +0900

    [Silver III] Title: N과 M (4), Time: 1504 ms, Memory: 314820 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] check;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new int[M];
+        check = new boolean[N];
+        dfs(0);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < M; i++) {
+                if (temp.isEmpty() || temp.get(temp.size() - 1) <= arr[i]) {
+                    temp.add(arr[i]);
+                } else return;
+            }
+            for (int i = 0; i < M; i++) {
+                System.out.print(temp.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i + 1;
+            dfs(depth + 1);
+        }
+    }
+}
