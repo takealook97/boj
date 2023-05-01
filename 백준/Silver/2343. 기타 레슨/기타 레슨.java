a202208596195951fbfc4f346bdb940d43b9d318
takealook <takealook97@naver.com>
2023-05-01 16:51:54 +0900

    [Silver I] Title: 기타 레슨, Time: 312 ms, Memory: 23800 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        int left = 0, right = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            left = Math.max(left, arr[i]);
+            right += arr[i];
+        }
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                if (sum + arr[i] > mid) {
+                    count++;
+                    sum = 0;
+                }
+                sum += arr[i];
+            }
+            if (sum != 0) count++;
+            if (count > M) left = mid + 1;
+            else right = mid - 1;
+        }
+        System.out.println(left);
+    }
+}
