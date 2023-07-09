a68e932b3287c20e7b3cc82f48a137dbfbac9f6d
takealook <takealook97@naver.com>
2023-07-10 06:36:07 +0900

    [Gold IV] Title: 키 순서, Time: 676 ms, Memory: 39880 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[][] arr;
+	static int INF = 987654321;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (i != j) {
+					arr[i][j] = INF;
+				}
+			}
+		}
+
+		while (M-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			arr[a][b] = 1;
+		}
+
+		for (int k = 1; k <= N; k++) {
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+				}
+			}
+		}
+
+		int count = 0;
+
+		for (int i = 1; i <= N; i++) {
+			boolean check = true;
+			for (int j = 1; j <= N; j++) {
+				if (arr[i][j] == INF && arr[j][i] == INF) {
+					check = false;
+					break;
+				}
+			}
+			if (check) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
