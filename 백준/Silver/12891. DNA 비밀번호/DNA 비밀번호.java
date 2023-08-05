45d058c29c6751e6e127cc29444de5b78a8be7fc
takealook <takealook97@naver.com>
2023-08-05 23:47:28 +0900

    [Silver II] Title: DNA 비밀번호, Time: 332 ms, Memory: 45400 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -5,50 +5,49 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
-        String arr = br.readLine();
-        st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        int G = Integer.parseInt(st.nextToken());
-        int T = Integer.parseInt(st.nextToken());
+	static int S, P, A, C, G, T;
+	static String password;
+	static HashMap<Character, Integer> alphabet = new HashMap<>();
+	static int count = 0;
 
-        int count = 0;
-        int head = 0;
-        int tail = P - 1;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		S = Integer.parseInt(st.nextToken());
+		P = Integer.parseInt(st.nextToken());
+		password = br.readLine();
+		st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		G = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
 
-        HashMap<Character, Integer> alphabet = new HashMap<>();
-        alphabet.put('A', 0);
-        alphabet.put('C', 0);
-        alphabet.put('G', 0);
-        alphabet.put('T', 0);
+		alphabet.put('A', 0);
+		alphabet.put('C', 0);
+		alphabet.put('G', 0);
+		alphabet.put('T', 0);
 
-        for (int j = head; j <= tail; j++) {//첫 윈도우 등록
-            if (alphabet.containsKey(arr.charAt(j))) {
-                alphabet.put(arr.charAt(j), alphabet.get(arr.charAt(j)) + 1);
-            }
-        }
-        if (alphabet.get('A') >= A && alphabet.get('C') >= C && alphabet.get('G') >= G && alphabet.get('T') >= T) {
-            count++;
-        }
+		int left = 0;
+		int right = P - 1;
 
-        for (int i = 1; i <= S - P; i++) {//슬라이딩
-            if (alphabet.containsKey(arr.charAt(head))) {//옆칸으로 이동 후 헤드 이동하면서 이전 헤드 맵에 포함시 삭제
-                alphabet.put(arr.charAt(head), alphabet.get(arr.charAt(head)) - 1);
-            }
-            head++;
-            tail++;
-            if (alphabet.containsKey(arr.charAt(tail))) {//추가된 인덱스가 맵에 포함될 시 추가
-                alphabet.put(arr.charAt(tail), alphabet.get(arr.charAt(tail)) + 1);
-            }
-            if (alphabet.get('A') >= A && alphabet.get('C') >= C && alphabet.get('G') >= G && alphabet.get('T') >= T) {
-                count++;
-            }
-        }
-        System.out.println(count);
-    }
+		for (int i = left; i <= right; i++) {
+			alphabet.put(password.charAt(i), alphabet.get(password.charAt(i)) + 1);
+		}
+		check();
+
+		for (int i = 1; i <= S - P; i++) {
+			alphabet.put(password.charAt(left), alphabet.get(password.charAt(left)) - 1);
+			left++;
+			right++;
+			alphabet.put(password.charAt(right), alphabet.get(password.charAt(right)) + 1);
+			check();
+		}
+		System.out.println(count);
+	}
+
+	static void check() {
+		if (alphabet.get('A') >= A && alphabet.get('C') >= C && alphabet.get('G') >= G && alphabet.get('T') >= T) {
+			count++;
+		}
+	}
 }
