10b41c15fd5abca2ef2d14289cafce5994f60388
takealook <takealook97@naver.com>
2023-08-09 11:57:58 +0900

    [Silver V] Title: 색종이, Time: 76 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,26 +1,34 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int[][] arr = new int[100][100];
+	static boolean[][] isExist;
+	static final int MAX_LENGTH = 100;
+	static final int PAPER_LENGTH = 10;
 
-        for (int i = 0; i < num; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            for (int j = x; j < x + 10; j++) {
-                for (int k = y; k < y + 10; k++) {
-                    arr[j][k] = 1;
-                }
-            }
-        }
-        int sum = 0;
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                sum += arr[i][j];
-            }
-        }
-        System.out.println(sum);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		isExist = new boolean[MAX_LENGTH][MAX_LENGTH];
+
+		for (int paper = 0; paper < num; paper++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			for (int i = x; i < x + PAPER_LENGTH; i++) {
+				for (int j = y; j < y + PAPER_LENGTH; j++) {
+					isExist[i][j] = true;
+				}
+			}
+		}
+		int sum = 0;
+		for (int i = 0; i < MAX_LENGTH; i++) {
+			for (int j = 0; j < MAX_LENGTH; j++) {
+				if (isExist[i][j]) {
+					sum++;
+				}
+			}
+		}
+		System.out.println(sum);
+	}
 }
