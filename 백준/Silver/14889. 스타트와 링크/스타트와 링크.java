571d014bd5cebbd1e67957a0df28e3cd33362ccb
takealook <takealook97@naver.com>
2023-08-10 11:27:35 +0900

    [Silver II] Title: 스타트와 링크, Time: 368 ms, Memory: 14984 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,55 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int[][] arr;
-    static boolean[] visit;
-    static int gap = 987654321;
+	static int N;
+	static int[][] board;
+	static boolean[] visit;
+	static int answer = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-        visit = new boolean[N];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		board = new int[N][N];
+		visit = new boolean[N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		dfs(0, 0);
+		System.out.println(answer);
+	}
 
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            StringTokenizer st = new StringTokenizer(line);
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        dfs(0, 0);
-        System.out.println(gap);
-    }
+	static void dfs(int index, int depth) {
+		if (depth == N / 2) {
+			int startTeam = 0;
+			int linkTeam = 0;
+			for (int i = 0; i < N - 1; i++) {
+				for (int j = i + 1; j < N; j++) {
+					if (visit[i] && visit[j]) {
+						startTeam += (board[i][j] + board[j][i]);
+					} else if (!visit[i] && !visit[j]) {
+						linkTeam += (board[i][j] + board[j][i]);
+					}
+				}
+			}
+			answer = Math.min(answer, Math.abs(startTeam - linkTeam));
+			return;
+		}
 
-    static void dfs(int index, int depth) {
-        if (depth == N / 2) {
-            int a = 0;
-            int b = 0;
-            for (int i = 0; i < N - 1; i++) {
-                for (int j = i + 1; j < N; j++) {
-                    if (visit[i] && visit[j]) {
-                        a += (arr[i][j] + arr[j][i]);
-                    } else if (!visit[i] && !visit[j]) {
-                        b += (arr[i][j] + arr[j][i]);
-                    }
-                }
-            }
-            gap = Math.min(gap, Math.abs(a - b));
-            return;
-        }
-        
-        for (int i = index; i < N; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                dfs(i + 1, depth + 1);
-                visit[i] = false;
-            }
-        }
-    }
+		for (int i = index; i < N; i++) {
+			if (!visit[i]) {
+				visit[i] = true;
+				dfs(i + 1, depth + 1);
+				visit[i] = false;
+			}
+		}
+	}
 }
