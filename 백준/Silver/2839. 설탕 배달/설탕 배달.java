2db3b99728c71edc87e5117ce4b9636c080accc7
takealook <takealook97@naver.com>
2023-08-11 11:54:56 +0900

    [Silver IV] Title: 설탕 배달, Time: 120 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -4,23 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int result = 0;
+        
         for (int i = 0; i <= N / 3; i++) {
             for (int j = 0; j <= N / 5; j++) {
-                if (N == 5 * j + 3 * i) {
-                    result = j + i;
-                    System.out.println(result);
-                    return;
+                if (N == i * 3 + j * 5) {
+                    System.out.println(i + j);
+                    System.exit(0);
                 }
             }
         }
-        for (int i = 0; i <= N / 3; i++) {
-            for (int j = 0; j <= N / 5; j++) {
-                if (N != 5 * j + 3 * i) {
-                    System.out.println(-1);
-                    return;
-                }
-            }
-        }
+        System.out.println(-1);
     }
 }
