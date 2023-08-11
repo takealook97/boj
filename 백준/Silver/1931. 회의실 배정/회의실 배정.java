a771f33518669d544762f742ea527c05af9de051
takealook <takealook97@naver.com>
2023-08-11 11:02:35 +0900

    [Silver I] Title: 회의실 배정, Time: 480 ms, Memory: 43128 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,31 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N][2];
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr, Comparator.comparingInt(o -> o[0]));
-        Arrays.sort(arr, Comparator.comparingInt(o -> o[1]));
-        //가장 빨리끝나면서 동시에 직전의 종료시각과 겹치지 않는 시간 찾기
-        int count = 1;
-        int end = arr[0][1];
-        for (int i = 1; i < N; i++) {
-            if (end <= arr[i][0]) {
-                end = arr[i][1];
-                count++;
-            }
-        }
-        System.out.println(count);
-    }
+	static class Room implements Comparable<Room> {
+		int start, end;
+
+		public Room(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		public int compareTo(Room o) {
+			if (this.end == o.end) {
+				return this.start - o.start;
+			}
+			return this.end - o.end;
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Room> list = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			list.add(new Room(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+		Collections.sort(list);
+		
+		int curEnd = list.get(0).end;
+		int answer = 1;
+
+		for (int i = 1; i < N; i++) {
+			Room nextRoom = list.get(i);
+			if (curEnd <= nextRoom.start) {
+				curEnd = nextRoom.end;
+				answer++;
+			}
+		}
+		System.out.println(answer);
+	}
 }
