0567ba346c99859733fe097086c7b8fc9110e2b3
takealook <takealook97@naver.com>
2023-08-21 13:29:35 +0900

    [Silver I] Title: 숨바꼭질, Time: 172 ms, Memory: 18092 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,45 +1,46 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	static int N, K;
+	static boolean[] visit;
+	static Queue<Integer> queue = new LinkedList<>();
+	static int count = 0;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        if (N == K) {
-            System.out.println(0);
-            System.exit(0);
-        }
-        boolean[] check = new boolean[100001];
-        check[N] = true;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(N);
-        int count = 0;
-        while (true) {
-            count++;
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int temp = queue.remove();
-                check[temp] = true;
-                if (temp - 1 == K || temp + 1 == K || temp * 2 == K) {
-                    System.out.println(count);
-                    System.exit(0);
-                }
-                if (temp - 1 >= 0 && !check[temp - 1]) {
-                    check[temp - 1] = true;
-                    queue.add(temp - 1);
-                }
-                if (temp + 1 <= 100000 && !check[temp + 1]) {
-                    check[temp + 1] = true;
-                    queue.add(temp + 1);
-                }
-                if (temp * 2 <= 100000 && !check[temp * 2]) {
-                    check[temp * 2] = true;
-                    queue.add(temp * 2);
-                }
-            }
-        }
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		K = sc.nextInt();
+		if (N == K) {
+			System.out.println(0);
+			System.exit(0);
+		}
+		visit = new boolean[100001];
+		visit[N] = true;
+		queue.add(N);
+		while (true) {
+			count++;
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				int cur = queue.poll();
+				visit[cur] = true;
+				if (cur - 1 == K || cur + 1 == K || cur * 2 == K) {
+					System.out.println(count);
+					System.exit(0);
+				}
+				if (cur - 1 >= 0 && !visit[cur - 1]) {
+					visit[cur - 1] = true;
+					queue.add(cur - 1);
+				}
+				if (cur + 1 <= 100000 && !visit[cur + 1]) {
+					visit[cur + 1] = true;
+					queue.add(cur + 1);
+				}
+				if (cur * 2 <= 100000 && !visit[cur * 2]) {
+					visit[cur * 2] = true;
+					queue.add(cur * 2);
+				}
+			}
+		}
+	}
 }
