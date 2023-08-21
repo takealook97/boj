7e7acca3f637f35ebd34e17b5737fd8f9cfa9129
takealook <takealook97@naver.com>
2023-08-21 12:13:04 +0900

    [Gold V] Title: 적록색약, Time: 100 ms, Memory: 12704 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,140 +1,101 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static char[][] arr;
-    static boolean[][] check;
-    static boolean[][] checkRG;
-    static int[] dy = {1, 0, -1, 0};
-    static int[] dx = {0, -1, 0, 1};
-    static int R;
-    static int G;
-    static int B;
-    static int RG;
+	static int N;
+	static char[][] normalBoard, blindBoard;
+	static boolean[][] visitNormal, visitBlind;
+	static int[] dy = { 1, 0, -1, 0 }, dx = { 0, -1, 0, 1 };
+	static int countNormal = 0;
+	static int countBlind = 0;
 
+	static class Point {
+		int y, x;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new char[N][N];
-        check = new boolean[N][N];
-        checkRG = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            String temp = br.readLine();
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = temp.charAt(j);
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (arr[i][j] == 'R' && !check[i][j]) {
-                    bfsR(i, j);
-                    R++;
-                } else if (arr[i][j] == 'G' && !check[i][j]) {
-                    bfsG(i, j);
-                    G++;
-                } else if (arr[i][j] == 'B' && !check[i][j]) {
-                    bfsB(i, j);
-                    B++;
-                }
-                if (arr[i][j] == 'R' || arr[i][j] == 'G') {
-                    if (!checkRG[i][j]) {
-                        bfsRG(i, j);
-                        RG++;
-                    }
-                }
-            }
-        }
-        System.out.println(R + G + B + " " + (RG + B));
-    }
+		public Point(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
 
-    static void bfsR(int y, int x) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{y, x});
-        while (!queue.isEmpty()) {
-            y = queue.peek()[0];
-            x = queue.peek()[1];
-            check[y][x] = true;
-            queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int cy = y + dy[i];
-                int cx = x + dx[i];
-                if (0 <= cy && cy < N && 0 <= cx && cx < N) {
-                    if (!check[cy][cx] && arr[cy][cx] == 'R') {
-                        queue.add(new int[]{cy, cx});
-                        check[cy][cx] = true;
-                    }
-                }
-            }
-        }
-    }
+		@Override
+		public String toString() {
+			return "Point [y=" + y + ", x=" + x + "]";
+		}
 
-    static void bfsG(int y, int x) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{y, x});
-        while (!queue.isEmpty()) {
-            y = queue.peek()[0];
-            x = queue.peek()[1];
-            check[y][x] = true;
-            queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int cy = y + dy[i];
-                int cx = x + dx[i];
-                if (0 <= cy && cy < N && 0 <= cx && cx < N) {
-                    if (!check[cy][cx] && arr[cy][cx] == 'G') {
-                        queue.add(new int[]{cy, cx});
-                        check[cy][cx] = true;
-                    }
-                }
-            }
-        }
-    }
+	}
 
-    static void bfsB(int y, int x) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{y, x});
-        while (!queue.isEmpty()) {
-            y = queue.peek()[0];
-            x = queue.peek()[1];
-            check[y][x] = true;
-            queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int cy = y + dy[i];
-                int cx = x + dx[i];
-                if (0 <= cy && cy < N && 0 <= cx && cx < N) {
-                    if (!check[cy][cx] && arr[cy][cx] == 'B') {
-                        queue.add(new int[]{cy, cx});
-                        check[cy][cx] = true;
-                    }
-                }
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		normalBoard = new char[N][N];
+		blindBoard = new char[N][N];
+		visitNormal = new boolean[N][N];
+		visitBlind = new boolean[N][N];
 
-    static void bfsRG(int y, int x) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{y, x});
-        while (!queue.isEmpty()) {
-            y = queue.peek()[0];
-            x = queue.peek()[1];
-            checkRG[y][x] = true;
-            queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int cy = y + dy[i];
-                int cx = x + dx[i];
-                if (0 <= cy && cy < N && 0 <= cx && cx < N) {
-                    if (!checkRG[cy][cx]) {
-                        if (arr[cy][cx] == 'R' || arr[cy][cx] == 'G') {
-                            queue.add(new int[]{cy, cx});
-                            checkRG[cy][cx] = true;
-                        }
-                    }
-                }
-            }
-        }
-    }
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < N; j++) {
+				char input = line.charAt(j);
+				normalBoard[i][j] = input;
+				if (input == 'G') {
+					input = 'R';
+				}
+				blindBoard[i][j] = input;
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (!visitNormal[i][j]) {
+					bfsNormal(i, j, normalBoard[i][j]);
+					countNormal++;
+				}
+				if (!visitBlind[i][j]) {
+					bfsBlind(i, j, blindBoard[i][j]);
+					countBlind++;
+				}
+			}
+		}
+		System.out.println(countNormal + " " + countBlind);
+	}
+
+	static void bfsBlind(int y, int x, char color) {
+		Queue<Point> queue = new LinkedList<>();
+		queue.add(new Point(y, x));
+		visitBlind[y][x] = true;
+
+		while (!queue.isEmpty()) {
+			Point now = queue.poll();
+			for (int i = 0; i < 4; i++) {
+				int nextY = now.y + dy[i];
+				int nextX = now.x + dx[i];
+				if (isInRange(nextY, nextX) && !visitBlind[nextY][nextX] && blindBoard[nextY][nextX] == color) {
+					queue.add(new Point(nextY, nextX));
+					visitBlind[nextY][nextX] = true;
+				}
+			}
+		}
+	}
+
+	static void bfsNormal(int y, int x, char color) {
+		Queue<Point> queue = new LinkedList<>();
+		queue.add(new Point(y, x));
+		visitNormal[y][x] = true;
+
+		while (!queue.isEmpty()) {
+			Point now = queue.poll();
+			for (int i = 0; i < 4; i++) {
+				int nextY = now.y + dy[i];
+				int nextX = now.x + dx[i];
+				if (isInRange(nextY, nextX) && !visitNormal[nextY][nextX] && normalBoard[nextY][nextX] == color) {
+					queue.add(new Point(nextY, nextX));
+					visitNormal[nextY][nextX] = true;
+				}
+			}
+		}
+	}
+
+	static boolean isInRange(int nextY, int nextX) {
+		return 0 <= nextY && nextY < N && 0 <= nextX && nextX < N;
+	}
 }
