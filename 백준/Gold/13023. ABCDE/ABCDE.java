72658ee9f37801140cefd06068a78f60fd6ca211
takealook <takealook97@naver.com>
2023-08-22 14:36:34 +0900

    [Gold V] Title: ABCDE, Time: 220 ms, Memory: 20008 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,13 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     static int N;
     static int M;
-    static ArrayList<Integer>[] arr;
+    static ArrayList<Integer>[] relationship;
     static boolean[] visit;
 
     public static void main(String[] args) throws IOException {
@@ -15,19 +12,19 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new ArrayList[N];
+        relationship = new ArrayList[N];
         visit = new boolean[N];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = new ArrayList<>();
+            relationship[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            arr[a].add(b);
-            arr[b].add(a);
+            relationship[a].add(b);
+            relationship[b].add(a);
         }
 
         for (int i = 0; i < N; i++) {
@@ -36,17 +33,17 @@ public class Main {
         System.out.println(0);
     }
 
-    static void dfs(int num, int depth) {
+    static void dfs(int idx, int depth) {
         if (depth == 5) {
-            System.out.println(1);
-            System.exit(0);
+        	System.out.println(1);
+        	System.exit(0);
         }
-        visit[num] = true;
-        for (int i : arr[num]) {
+        visit[idx] = true;
+        for (int i : relationship[idx]) {
             if (!visit[i]) {
                 dfs(i, depth + 1);
             }
         }
-        visit[num] = false;
+        visit[idx] = false;
     }
 }
