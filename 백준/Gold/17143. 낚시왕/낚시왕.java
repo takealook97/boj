c31c6fa8a1010f5df871fc1b955b15ad7e13ff4f
takealook <takealook97@naver.com>
2023-08-24 22:14:52 +0900

    [Gold I] Title: 낚시왕, Time: 312 ms, Memory: 46808 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -1,101 +1,92 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int R, C, M;
-    static int fishermanPos = -1;
-    static Shark[][] sharkMap;
-    static int[] dy = {-1, 1, 0, 0}; // 위, 아래, 오른쪽, 왼쪽
-    static int[] dx = {0, 0, 1, -1};
-    static int sizeSum = 0;
+	static int R, C, M;
+	static int fishermanPos = -1;
+	static Shark[][] board;
+	static int[] dy = {-1, 1, 0, 0}, dx = {0, 0, 1, -1};//상 하 우 좌
+	static int sizeSum = 0;
 
-    static class Shark {
-        int speed, dir, size;
+	static class Shark {
+		int speed, dir, size;
 
-        public Shark(int speed, int dir, int size) {
-            this.speed = speed;
-            this.dir = dir;
-            this.size = size;
-        }
-    }
+		public Shark(int speed, int dir, int size) {
+			this.speed = speed;
+			this.dir = dir;
+			this.size = size;
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		board = new Shark[R][C];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int y = Integer.parseInt(st.nextToken()) - 1;
+			int x = Integer.parseInt(st.nextToken()) - 1;
+			board[y][x] = new Shark(
+				Integer.parseInt(st.nextToken()),
+				Integer.parseInt(st.nextToken()) - 1,
+				Integer.parseInt(st.nextToken())
+			);
+		}
 
-        sharkMap = new Shark[R][C];
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int y = Integer.parseInt(st.nextToken()) - 1;
-            int x = Integer.parseInt(st.nextToken()) - 1;
-            sharkMap[y][x] = new Shark(
-                    Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken()) - 1,
-                    Integer.parseInt(st.nextToken())
-            );
-        }
+		for (int i = 0; i < C; i++) {
+			fishermanPos++;
+			catchShark();
+			moveSharks();
+		}
+		System.out.println(sizeSum);
+	}
 
-        for (int i = 0; i < C; i++) {
-        	fishermanPos++;
-            catchShark();
-            moveSharks();
-        }
-        System.out.println(sizeSum);
-    }
-
-    static void catchShark() {
-        for (int y = 0; y < R; y++) {
-            if (sharkMap[y][fishermanPos] != null) {
-                sizeSum += sharkMap[y][fishermanPos].size;
-                sharkMap[y][fishermanPos] = null;
-                return;
-            }
-        }
-    }
-
-    static void moveSharks() {
-        Shark[][] updatedSharkMap = new Shark[R][C];
-        for (int y = 0; y < R; y++) {
-            for (int x = 0; x < C; x++) {
-                if (sharkMap[y][x] != null) {
-                    Shark shark = sharkMap[y][x];
-                    int[] newPos = getNextPos(y, x, shark);
-                    int nextY = newPos[0];
-                    int nextX = newPos[1];
-                    if (updatedSharkMap[nextY][nextX] == null || updatedSharkMap[nextY][nextX].size < shark.size) {
-                        updatedSharkMap[nextY][nextX] = new Shark(shark.speed, newPos[2], shark.size);
-                    }
-                }
-            }
-        }
-        sharkMap = updatedSharkMap;
-    }
-
-    static int[] getNextPos(int curY, int curX, Shark shark) {
-        int curSpeed = shark.speed;
-        int curDir = shark.dir;
-
-        while (curSpeed-- > 0) {
-			int nextY = curY + dy[curDir];
-			int nextX = curX + dx[curDir];
-			if (0 <= nextY && nextY < R && 0 <= nextX && nextX < C) {
-				curY = nextY;
-				curX = nextX;
-			} else {
-				if (curDir % 2 == 0) {
-					curDir++;
-				} else {
-					curDir--;
-				}
-				curY += dy[curDir];
-				curX += dx[curDir];
+	static void catchShark() {
+		for (int y = 0; y < R; y++) {
+			if (board[y][fishermanPos] != null) {
+				sizeSum += board[y][fishermanPos].size;
+				board[y][fishermanPos] = null;
+				return;
 			}
 		}
-        return new int[]{curY, curX, curDir};
-    }
+	}
+
+	static void moveSharks() {
+		Shark[][] updatedBoard = new Shark[R][C];
+		for (int y = 0; y < R; y++) {
+			for (int x = 0; x < C; x++) {
+				if (board[y][x] != null) {
+					Shark shark = board[y][x];
+					int nextY = y + shark.speed * dy[shark.dir];
+					int nextX = x + shark.speed * dx[shark.dir];
+
+					while (nextY < 0 || nextY >= R || nextX < 0 || nextX >= C) {
+						if (nextY < 0) {
+							shark.dir = 1;
+							nextY = -nextY;
+						} else if (nextY >= R) {
+							shark.dir = 0;
+							nextY = 2 * R - 2 - nextY;
+						}
+						
+						if (nextX < 0) {
+							shark.dir = 2;
+							nextX = -nextX;
+						} else if (nextX >= C) {
+							shark.dir = 3;
+							nextX = 2 * C - 2 - nextX;
+						}
+					}
+
+					if (updatedBoard[nextY][nextX] == null || updatedBoard[nextY][nextX].size < shark.size) {
+						updatedBoard[nextY][nextX] = new Shark(shark.speed, shark.dir, shark.size);
+					}
+				}
+			}
+		}
+		board = updatedBoard;
+	}
 }
