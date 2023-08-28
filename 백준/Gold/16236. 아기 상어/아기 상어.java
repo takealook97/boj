6a58e653336171212b3dc3025050598df143c3f6
takealook <takealook97@naver.com>
2023-08-28 11:50:54 +0900

    [Gold III] Title: 아기 상어, Time: 92 ms, Memory: 12248 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,96 +1,115 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[][] arr;
-    static int[] dy = {0, 0, -1, 1};
-    static int[] dx = {-1, 1, 0, 0};
-    static int size = 2;
-    static int stomach = 0;
-    static int result = 0;
+	static int N;
+	static int[][] board;
+	static boolean[][] visit;
+	static BabyShark babyShark;
+	static int[] dy = {-1, 1, 0, 0}, dx = {0, 0, -1, 1};
+	
+	static class BabyShark {
+		int y, x, size, gauge;
 
-    static class Node {
-        int y;
-        int x;
-        int distance;
+		public BabyShark(int y, int x, int size, int gauge) {
+			this.y = y;
+			this.x = x;
+			this.size = size;
+			this.gauge = gauge;
+		}
+	}
+	
+	static class Fish implements Comparable<Fish>{
+		int y, x, size, dist;
+		
 
-        public Node(int y, int x, int distance) {
-            this.y = y;
-            this.x = x;
-            this.distance = distance;
-        }
-    }
+		public Fish(int y, int x, int size, int dist) {
+			this.y = y;
+			this.x = x;
+			this.size = size;
+			this.dist = dist;
+		}
 
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-        Queue<Node> queue = new LinkedList<>();
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                if (arr[i][j] == 9) {
-                    queue.add(new Node(i, j, 0));
-                    arr[i][j] = 0;
-                }
-            }
-        }
-
-        while (true) {
-            ArrayList<Node> preys = new ArrayList<>();
-            int[][] distance = new int[N][N];
-            while (!queue.isEmpty()) {
-                Node now = queue.poll();
-                int nowY = now.y;
-                int nowX = now.x;
-                for (int i = 0; i < 4; i++) {
-                    int nextY = nowY + dy[i];
-                    int nextX = nowX + dx[i];
-                    if (0 <= nextY && nextY < N && 0 <= nextX && nextX < N) {
-                        if (distance[nextY][nextX] == 0 && arr[nextY][nextX] <= size) {
-                            distance[nextY][nextX] = distance[nowY][nowX] + 1;
-                            queue.add(new Node(nextY, nextX, distance[nextY][nextX]));
-                            if (1 <= arr[nextY][nextX] && arr[nextY][nextX] <= 6 && arr[nextY][nextX] < size) {
-                                preys.add(new Node(nextY, nextX, distance[nextY][nextX]));
-                            }
-                        }
-                    }
-                }
-            }
-            if (preys.size() == 0) {
-                System.out.println(result);
-                System.exit(0);
-            }
-            Node now = preys.get(0);
-            for (int i = 1; i < preys.size(); i++) {
-                if (now.distance > preys.get(i).distance) {
-                    now = preys.get(i);
-                } else if (now.distance == preys.get(i).distance) {
-                    if (now.y > preys.get(i).y) {
-                        now = preys.get(i);
-                    } else if (now.y == preys.get(i).y) {
-                        if (now.x > preys.get(i).x) {
-                            now = preys.get(i);
-                        }
-                    }
-                }
-            }
-            arr[now.y][now.x] = 0;
-            result += now.distance;
-            stomach++;
-            if (size == stomach) {
-                stomach = 0;
-                size++;
-            }
-            queue.add(now);
-        }
-    }
+		@Override
+		public int compareTo(Fish o) {
+			if(this.dist == o.dist) {
+				if(this.y == o.y) {
+					return this.x - o.x;
+				}
+				return this.y - o.y;
+			}
+			return this.dist - o.dist;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	   N = Integer.parseInt(br.readLine());
+	   board = new int[N][N];
+	   for(int i = 0; i < N; i++) {
+		   StringTokenizer st = new StringTokenizer(br.readLine());
+		   for(int j = 0; j < N ; j++) {
+			   board[i][j] = Integer.parseInt(st.nextToken());
+			   if(board[i][j] == 9) {
+				   babyShark = new BabyShark(i, j, 2, 0);
+			   }
+		   }
+	   }
+	   
+	   int time = 0;
+	   while(true) {
+		   Fish prey = getPrey();
+		   if(prey == null) {
+			   break;//break
+		   }
+		   //먹이가 있다면
+		   int nextGauge = babyShark.gauge + 1;
+		   board[babyShark.y][babyShark.x] = 0;
+		   if(nextGauge == babyShark.size) {
+			   babyShark = new BabyShark(prey.y, prey.x, babyShark.size + 1, 0);
+		   } else {
+			   babyShark = new BabyShark(prey.y, prey.x, babyShark.size, babyShark.gauge + 1);
+		   }
+		   board[prey.y][prey.x] = 9;
+		   time += prey.dist;
+	   }
+	   System.out.println(time);
+	}
+	
+	static Fish getPrey() {
+		PriorityQueue<Fish> pq = new PriorityQueue<>();
+		visit = new boolean[N][N];
+		pq.add(new Fish(babyShark.y, babyShark.x, 0, 0));
+		visit[babyShark.y][babyShark.x] = true;
+		Fish prey = null;
+		while(!pq.isEmpty()) {
+			Fish now = pq.poll();
+			if(isFish(now.y, now.x)) {
+				if(board[now.y][now.x] < babyShark.size) {
+					prey = now;
+					break;
+				} else if(board[now.y][now.x] > babyShark.size){
+					continue;
+				}
+			}
+			
+			for(int i = 0; i < 4; i++) {
+				int nextY = now.y + dy[i];
+				int nextX = now.x + dx[i];
+				if(isMovable(nextY, nextX)) {
+					visit[nextY][nextX] = true;
+					pq.add(new Fish(nextY, nextX, board[nextY][nextX], now.dist + 1));
+				}
+			}
+		}
+		return prey;
+	}
+	
+	static boolean isFish(int y, int x) {
+		return board[y][x] != 0 && board[y][x] != 9;
+	}
+	
+	static boolean isMovable(int y, int x) {
+		return 0 <= y && y < N && 0 <= x && x < N && !visit[y][x] && board[y][x] <= babyShark.size;
+	}
 }
