2180fff77ed0f0f624cf95d05aeb6d5f05601b3a
takealook <takealook97@naver.com>
2023-08-28 20:57:35 +0900

    [Gold IV] Title: 게리맨더링, Time: 100 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,117 +1,123 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * @author 김건우
+ *
+ *
+ * 1. 게리맨더링 -> 50개의 test 3초
+ * 2. N 개 마을을 2개 지역구로 분리
+ * 3. 정확하게 두개의 트리로 구분되어야 한다.
+ * 4. 2 개 지역구 유권자 수 차이의 최소값을 출력
+ * 5. 4 <= N <= 8
+ * 6. 유권자 수 0 <= Pi <= 20
+ * 7. 인접할경우 1, 아닐경우 0
+ * 8. 간선 방향 X
+ *
+ */
+
 public class Main {
 	static int N;
-	static int[] peopleCount;
-	static int sum = 0;
-	static int treeSum = 0;
-	static ArrayList<Integer>[] linkArr;
-	static boolean[] visitSrc;
+	static ArrayList<Integer>[] listArr;//간선 정보 인접리스트
+	static int[] population;
 	static boolean[] visit;
-	static int M;
-	static int answer = Integer.MAX_VALUE;
+	static int allSum, targetCount;
+	static int answer;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		linkArr = new ArrayList[N + 1];
+		N = Integer.parseInt(br.readLine().trim());
+		listArr = new ArrayList[N + 1];
+		population = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
-			linkArr[i] = new ArrayList<>();
+			listArr[i] = new ArrayList<>();
 		}
+
+		allSum = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		peopleCount = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
-			peopleCount[i] = Integer.parseInt(st.nextToken());
-			sum += peopleCount[i];
+			population[i] = Integer.parseInt(st.nextToken());
+			allSum += population[i];
 		}
-		for (int from = 1; from <= N; from++) {
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int linkCount = Integer.parseInt(st.nextToken());
-			while (linkCount-- > 0) {
-				int to = Integer.parseInt(st.nextToken());
-				if(!linkArr[from].contains(to)) {
-					linkArr[from].add(to);
-				}
-				if(!linkArr[to].contains(from)) {
-					linkArr[to].add(from);
-				}
+			int count = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < count; j++) {
+				listArr[i].add(Integer.parseInt(st.nextToken()));
 			}
 		}
-		
-		visitSrc = new boolean[N + 1];
-		visit = new boolean[N + 1];
-		
-		for(int i = 1; i <= N / 2; i++) {
-			M = i;
-			comb(1, 0);
+
+		answer = Integer.MAX_VALUE;//answer 초기화
+
+		//마을은 모두 이어져있다. -> 1부터 시작
+		for (int i = 1; i <= N / 2; i++) {
+			if (answer == 0) {
+				break;
+			}
+			targetCount = i;
+			visit = new boolean[N + 1];
+			getMinGap(1, 0, 0);
 		}
-			
-		if(answer == Integer.MAX_VALUE) {
+		if (answer == Integer.MAX_VALUE) {
 			System.out.println(-1);
 		} else {
 			System.out.println(answer);
 		}
 	}
-	
-	static void comb(int idx, int depth) {
-		if(depth == M) {
-			if(isTree(true) && isTree(false)) {
-				answer = Math.min(answer, getPopulationGap());
+
+	//조합을 통해 visit을 해버리고 visit이 아닌애들끼리 묶이는지 확인
+	static void getMinGap(int now, int sum, int depth) {
+		if (depth == targetCount) {
+			//true인 애들과 false인 애들 각각 비교
+			boolean isTruePassed = false;
+			boolean isFalsePassed = false;
+			for (int i = 1; i <= N; i++) {
+				if (visit[i]) {
+					isTruePassed = isLinked(i, true);
+					break;
+				}
+			}
+			for (int i = 1; i <= N; i++) {
+				if (!visit[i]) {
+					isFalsePassed = isLinked(i, false);
+					break;
+				}
+			}
+			if (isTruePassed && isFalsePassed) {
+				answer = Math.min(answer, Math.abs((allSum - sum) - sum));
 			}
 			return;
 		}
-		for(int i = idx; i <= N; i++) {
-			if(!visitSrc[i]) {
-				visitSrc[i] = true;
-				comb(idx + 1, depth + 1);
-				visitSrc[i] = false;
+		for (int i = now; i <= N; i++) {
+			if (!visit[i]) {
+				visit[i] = true;
+				getMinGap(i + 1, sum + population[i], depth + 1);
+				visit[i] = false;
 			}
 		}
 	}
-	
-	static boolean isTree(boolean status) {
-		visit = Arrays.copyOf(visitSrc, N + 1);
-		int startNode = -1;
-		
-		for(int i = 1; i <= N; i++) {
-			if(visit[i] == status) {
-				startNode = i;
-				break;
-			}
-		}
-		return isAllLinked(startNode, status);
-	}
-	
-	static boolean isAllLinked(int startNode, boolean status) {
+
+	static boolean isLinked(int idx, boolean status) {
 		Queue<Integer> queue = new LinkedList<>();
-		queue.add(startNode);
-		treeSum = peopleCount[startNode];
-		visit[startNode] = !status;
-		while(!queue.isEmpty()) {
+		queue.add(idx);
+		boolean[] tempVisit = Arrays.copyOf(visit, N + 1);
+		tempVisit[idx] = !status;
+		while (!queue.isEmpty()) {
 			int now = queue.poll();
-			if(!linkArr[now].isEmpty()) {
-				for(int next : linkArr[now]) {
-					if(visit[next] == status) {
-						visit[next] = !status;
-						treeSum += peopleCount[next];
+			if (!listArr[now].isEmpty()) {
+				for (int next : listArr[now]) {
+					if (tempVisit[next] == status) {
+						tempVisit[next] = !status;
 						queue.add(next);
 					}
 				}
 			}
 		}
-		for(int i = 1; i < N; i++) {
-			if(visit[i] != visit[i + 1]) {
+		for (int i = 1; i < N; i++) {
+			if (tempVisit[i] != tempVisit[i + 1]) {
 				return false;
 			}
 		}
 		return true;
-	}
-	
-	static int getPopulationGap() {
-		int aSum = sum - treeSum;
-		int bSum = treeSum;
-		return Math.abs(aSum - bSum);
 	}
 }
