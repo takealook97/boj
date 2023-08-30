0e542ee1dbb0d34ba5ade5ccd63c3f1f45766a7b
takealook <takealook97@naver.com>
2023-08-30 12:01:46 +0900

    [Silver V] Title: 다리 놓기, Time: 88 ms, Memory: 12876 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,24 +1,31 @@
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        int[] N = new int[T];
-        int[] M = new int[T];
-        for (int i = 0; i < T; i++) {
-            N[i] = sc.nextInt();
-            M[i] = sc.nextInt();
-            if (M[i] - N[i] <= N[i]) {
-                N[i] = M[i] - N[i];
-            }
-            long a = 1;
-            long b = 1;
-            for (int j = 0; j < N[i]; j++) {
-                a *= (M[i] - j);
-                b *= (N[i] - j);
-            }
-            System.out.println(a / b);
-        }
-    }
+	static int T, n, m;
+	static int[][] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		while (T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			n = Math.min(n, m - n);
+			dp = new int[m + 1][m + 1];
+			
+			for (int i = 1; i <= m; i++) {
+				for (int j = 0; j <= n; j++) {
+					if (j == 0 || j == i) {
+						dp[i][j] = 1;
+					} else {
+						dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+					}
+				}
+			}
+			sb.append(dp[m][n]).append("\n");
+		}
+		System.out.print(sb);
+	}
 }
