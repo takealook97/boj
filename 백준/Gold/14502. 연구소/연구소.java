b25d612bc6d5e2cea7afdc008eda68b99df04921
takealook <takealook97@naver.com>
2023-09-25 12:25:32 +0900

    [Gold IV] Title: 연구소, Time: 508 ms, Memory: 144780 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,21 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N, M;
-    static int[][] arr;
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
-
-    static int result = 0;
-
-    static ArrayList<Point> virus = new ArrayList<>();
-    static ArrayList<Point> blank = new ArrayList<>();
+    static int[][] board;
+    static boolean[][] visit;
+    static int[] dy = {-1, 1, 0, 0}, dx = {0, 0, -1, 1};
+    static ArrayList<Point> virusList = new ArrayList<>();
+    static ArrayList<Point> blankList = new ArrayList<>();
+    static int answer = 0;
 
     static class Point {
         int y, x;
@@ -31,52 +24,50 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N][M];
+        board = new int[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                if (arr[i][j] == 2) {
-                    virus.add(new Point(i, j));
-                } else if (arr[i][j] == 0) {
-                    blank.add(new Point(i, j));
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if (board[i][j] == 2) {
+                    virusList.add(new Point(i, j));
+                } else if (board[i][j] == 0) {
+                    blankList.add(new Point(i, j));
                 }
             }
         }
 
-        for (int i = 0; i < blank.size() - 2; i++) {
-            for (int j = i + 1; j < blank.size() - 1; j++) {
-                for (int k = j + 1; k < blank.size(); k++) {
-                    Point[] walls = {blank.get(i), blank.get(j), blank.get(k)};
+        for (int i = 0; i < blankList.size() - 2; i++) {
+            for (int j = i + 1; j < blankList.size() - 1; j++) {
+                for (int k = j + 1; k < blankList.size(); k++) {
+                    Point[] walls = {blankList.get(i), blankList.get(j), blankList.get(k)};
                     bfs(walls);
                 }
-            }
+            }	
         }
-        System.out.println(result);
+        System.out.println(answer);
     }
 
 
     static void bfs(Point[] walls) {
         int[][] temp = new int[N][M];
         for (int i = 0; i < N; i++) {
-            if (M >= 0) System.arraycopy(arr[i], 0, temp[i], 0, M);
+            if (M >= 0) System.arraycopy(board[i], 0, temp[i], 0, M);// 깊은 복사로 가져온다.
         }
         for (Point point : walls) {
             temp[point.y][point.x] = 1;
         }
-        boolean[][] visit = new boolean[N][M];
+        visit = new boolean[N][M];
         Queue<Point> queue = new LinkedList<>();
-        for (Point point : virus) {
+        for (Point point : virusList) {
             visit[point.y][point.x] = true;
             queue.add(new Point(point.y, point.x));
         }
         while (!queue.isEmpty()) {
             Point now = queue.poll();
-            int nowY = now.y;
-            int nowX = now.x;
             for (int i = 0; i < 4; i++) {
-                int nextY = nowY + dy[i];
-                int nextX = nowX + dx[i];
+                int nextY = now.y + dy[i];
+                int nextX = now.x + dx[i];
                 if (0 <= nextY && nextY < N && 0 <= nextX && nextX < M && !visit[nextY][nextX] && temp[nextY][nextX] == 0) {
                     visit[nextY][nextX] = true;
                     temp[nextY][nextX] = 2;
@@ -84,19 +75,15 @@ public class Main {
                 }
             }
         }
-        result = Math.max(result, getSafeZone(temp));
-    }
 
-
-    static int getSafeZone(int[][] temp) {
-        int count = 0;
+        int safeZoneCount = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (temp[i][j] == 0) {
-                    count++;
+                    safeZoneCount++;
                 }
             }
         }
-        return count;
+        answer = Math.max(answer, safeZoneCount);
     }
 }
