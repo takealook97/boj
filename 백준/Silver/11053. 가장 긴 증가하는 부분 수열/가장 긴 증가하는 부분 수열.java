968d18237e1397c336ab091b7cd24ccbaa3d2410
takealook <takealook97@naver.com>
2023-09-26 10:26:03 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 96 ms, Memory: 11892 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,32 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] A = new int[N];
-        int[] LIS = new int[N];
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-            LIS[i] = 1;
-        }
-
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < i; j++) {
-                if (A[j] < A[i] && LIS[i] < LIS[j] + 1) {
-                    LIS[i] = LIS[j] + 1;
-                }
-            }
-        }
-
-        int result = 1;
-        for (int i = 0; i < N; i++) {
-            result = Math.max(LIS[i], result);
-        }
-        System.out.println(result);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		int[] LIS = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.fill(LIS, 1);
+		int answer = 1;
+		for (int i = 1; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i] && LIS[i] < LIS[j] + 1) {
+					LIS[i] = LIS[j] + 1;
+					answer = Math.max(answer, LIS[i]);
+				}
+			}
+		}
+		System.out.println(answer);
+	}
 }
