25c67e1ef1f174966caf8f1d7d36b984e94b1dce
takealook <takealook97@naver.com>
2023-10-10 11:37:55 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 500 ms, Memory: 146848 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -2,12 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[][] arr;
 	static int R, C, T;
-	static int[] dy = {-1, 1, 0, 0};
-	static int[] dx = {0, 0, -1, 1};
-	static Point upP;
-	static Point downP;
+	static int[][] board;
+	static int[] dy = { -1, 1, 0, 0 }, dx = { 0, 0, -1, 1 };
+	static Point upside, downside;
+	static final int PURIFIER = -1;
 
 	static class Point {
 		int y, x;
@@ -24,22 +23,26 @@ public class Main {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		T = Integer.parseInt(st.nextToken());
-		arr = new int[R][C];
+		board = new int[R][C];
 		for (int i = 0; i < R; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < C; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				if (arr[i][j] == -1 && upP == null) {
-					upP = new Point(i, j);
-				} else if (arr[i][j] == -1 && upP != null && downP == null) {
-					downP = new Point(i, j);
+				board[i][j] = Integer.parseInt(st.nextToken());
+				if(board[i][j] == PURIFIER) {
+					if (upside == null) {
+						upside = new Point(i, j);
+					} else if (upside != null && downside == null) {
+						downside = new Point(i, j);
+					}
 				}
 			}
 		}
+
 		while (T-- > 0) {
 			spread();
 			purify();
 		}
+
 		System.out.println(getDust());
 	}
 
@@ -48,7 +51,7 @@ public class Main {
 		Queue<Point> queue = new LinkedList<>();
 		for (int i = 0; i < R; i++) {
 			for (int j = 0; j < C; j++) {
-				if (arr[i][j] > 0) {
+				if (board[i][j] > 0) {
 					queue.add(new Point(i, j));
 				}
 			}
@@ -56,64 +59,66 @@ public class Main {
 
 		while (!queue.isEmpty()) {
 			Point now = queue.poll();
-			int nowY = now.y;
-			int nowX = now.x;
-			int part = arr[nowY][nowX] / 5;
-			int count = 0;
+			int part = board[now.y][now.x] / 5;
+			int addCount = 0;
 			for (int i = 0; i < 4; i++) {
-				int nextY = nowY + dy[i];
-				int nextX = nowX + dx[i];
-				if (0 <= nextY && nextY < R && 0 <= nextX && nextX < C && arr[nextY][nextX] != -1) {
+				int nextY = now.y + dy[i];
+				int nextX = now.x + dx[i];
+				if (isMovable(nextY, nextX)) {
 					plus[nextY][nextX] += part;
-					count++;
+					addCount++;
 				}
 			}
-			arr[nowY][nowX] -= (part * count);
+			board[now.y][now.x] -= (part * addCount);
 		}
 
 		for (int i = 0; i < R; i++) {
 			for (int j = 0; j < C; j++) {
-				arr[i][j] += plus[i][j];
+				board[i][j] += plus[i][j];
 			}
 		}
 	}
 
 	static void purify() {
-		for (int i = upP.y - 1; i > 0; i--) {
-			arr[i][0] = arr[i - 1][0];
+		for (int i = upside.y - 1; i > 0; i--) {
+			board[i][0] = board[i - 1][0];
 		}
 		for (int i = 0; i < C - 1; i++) {
-			arr[0][i] = arr[0][i + 1];
+			board[0][i] = board[0][i + 1];
 		}
-		for (int i = 0; i < upP.y; i++) {
-			arr[i][C - 1] = arr[i + 1][C - 1];
+		for (int i = 0; i < upside.y; i++) {
+			board[i][C - 1] = board[i + 1][C - 1];
 		}
 		for (int i = C - 1; i > 1; i--) {
-			arr[upP.y][i] = arr[upP.y][i - 1];
+			board[upside.y][i] = board[upside.y][i - 1];
 		}
-		arr[upP.y][1] = 0;
+		board[upside.y][1] = 0;
 
-		for (int i = downP.y + 1; i < R - 1; i++) {
-			arr[i][0] = arr[i + 1][0];
+		for (int i = downside.y + 1; i < R - 1; i++) {
+			board[i][0] = board[i + 1][0];
 		}
 		for (int i = 0; i < C - 1; i++) {
-			arr[R - 1][i] = arr[R - 1][i + 1];
+			board[R - 1][i] = board[R - 1][i + 1];
 		}
-		for (int i = R - 1; i > downP.y; i--) {
-			arr[i][C - 1] = arr[i - 1][C - 1];
+		for (int i = R - 1; i > downside.y; i--) {
+			board[i][C - 1] = board[i - 1][C - 1];
 		}
 		for (int i = C - 1; i > 1; i--) {
-			arr[downP.y][i] = arr[downP.y][i - 1];
+			board[downside.y][i] = board[downside.y][i - 1];
 		}
-		arr[downP.y][1] = 0;
+		board[downside.y][1] = 0;
+	}
+
+	static boolean isMovable(int y, int x) {
+		return 0 <= y && y < R && 0 <= x && x < C && board[y][x] != PURIFIER;
 	}
 
 	static int getDust() {
 		int result = 0;
 		for (int i = 0; i < R; i++) {
 			for (int j = 0; j < C; j++) {
-				if (arr[i][j] != -1) {
-					result += arr[i][j];
+				if (board[i][j] != PURIFIER) {
+					result += board[i][j];
 				}
 			}
 		}
