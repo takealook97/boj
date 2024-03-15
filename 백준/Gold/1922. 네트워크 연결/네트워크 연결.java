a26fe36b2edf3d9d54b760ebeaebd50ef3e365d3
takealook97 <takealook97@naver.com>
2024-03-15 09:29:14 +0900

    [Gold IV] Title: 네트워크 연결, Time: 348 ms, Memory: 45188 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -3,15 +3,16 @@ import java.util.*;
 
 public class Main {
 	static int N, M;
-	static ArrayList<Edge>[] listArr;
-	static boolean[] visited;
+	static int[] parent;
+	static PriorityQueue<Edge> pq = new PriorityQueue<>();
 	static int answer = 0;
 
 	static class Edge implements Comparable<Edge> {
-		int node, dist;
+		int from, to, dist;
 
-		public Edge(int node, int dist) {
-			this.node = node;
+		public Edge(int from, int to, int dist) {
+			this.from = from;
+			this.to = to;
 			this.dist = dist;
 		}
 
@@ -25,46 +26,54 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
-		listArr = new ArrayList[N + 1];
-		visited = new boolean[N + 1];
-		for (int i = 0; i <= N; i++) {
-			listArr[i] = new ArrayList<>();
-		}
+
+		parent = new int[N + 1];
 
 		for (int i = 0; i < M; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			listArr[from].add(new Edge(to, weight));
-			listArr[to].add(new Edge(from, weight));
+			int dist = Integer.parseInt(st.nextToken());
+			pq.add(new Edge(from, to, dist));
 		}
 
-		getMinDist();
+		make();
+
+		int count = 0;
+		while (!pq.isEmpty()) {
+			Edge edge = pq.poll();
+			if (union(edge.from, edge.to)) {
+				answer += edge.dist;
+				count++;
+				if (count == N - 1) {
+					break;
+				}
+			}
+		}
 
 		System.out.println(answer);
 	}
 
-	static void getMinDist() {
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		pq.add(new Edge(1, 0));
-
-		while (!pq.isEmpty()) {
-			Edge now = pq.poll();
-			if (!visited[now.node]) {
-				visited[now.node] = true;
-				answer += now.dist;
-
-				if (listArr[now.node].isEmpty()) {
-					continue;
-				}
-
-				for (Edge next : listArr[now.node]) {
-					if (!visited[next.node]) {
-						pq.add(next);
-					}
-				}
-			}
+	static void make() {
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
 		}
+	}
+
+	static boolean union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		if (x != y) {
+			parent[y] = x;
+			return true;
+		}
+		return false;
+	}
+
+	static int find(int x) {
+		if (parent[x] == x) {
+			return x;
+		}
+		return parent[x] = find(parent[x]);
 	}
 }
