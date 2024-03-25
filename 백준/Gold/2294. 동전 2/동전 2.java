dda7a4a47fbf0342630cff84314b034b01d23c41
takealook97 <takealook97@naver.com>
2024-03-26 00:20:12 +0900

    [Gold V] Title: 동전 2, Time: 96 ms, Memory: 11752 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, k;
+	static int[] arr, dp;
+	static final int SIZE = 10000;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		arr = new int[n];
+		dp = new int[k + 1];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.fill(dp, SIZE + 1);
+		dp[0] = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = arr[i]; j <= k; j++) {
+				dp[j] = Math.min(dp[j], dp[j - arr[i]] + 1);
+			}
+		}
+
+		if (dp[k] > SIZE) {
+			System.out.println(-1);
+		} else {
+			System.out.println(dp[k]);
+		}
+	}
+}
