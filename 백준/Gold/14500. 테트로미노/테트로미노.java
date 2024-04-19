7a9e3325e0165bed356c71053c9e44962f4c696f
takealook97 <takealook97@naver.com>
2024-04-19 13:29:16 +0900

    [Gold IV] Title: 테트로미노, Time: 828 ms, Memory: 57364 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,82 +1,100 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[][] arr;
-    static boolean[][] visit;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static int result;
+	static int N, M;
+	static int[][] board;
+	static boolean[][] visited;
+	static int[] dy = {-1, 1, 0, 0}, dx = {0, 0, -1, 1};
+	static ArrayList<Point> list = new ArrayList<>();
+	static int answer = 0;
 
+	static class Point {
+		int y, x;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());//y
-        M = Integer.parseInt(st.nextToken());//x
-        arr = new int[N][M];
-        visit = new boolean[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        result = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                dfs(i, j, 0, 0);
-                bfs(i, j);
-            }
-        }
-        System.out.println(result);
-    }
+		public Point(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
+	}
 
-    static void dfs(int y, int x, int depth, int sum) {
-        if (depth == 4) {
-            if (result < sum) {
-                result = sum;
-            }
-            return;
-        }
-        for (int i = 0; i < 4; i++) {
-            int nextY = y + dy[i];
-            int nextX = x + dx[i];
-            if (0 <= nextY && nextY < N && 0 <= nextX && nextX < M) {
-                if (!visit[nextY][nextX]) {
-                    visit[nextY][nextX] = true;
-                    int nextSum = sum + arr[nextY][nextX];
-                    dfs(nextY, nextX, depth + 1, nextSum);
-                    visit[nextY][nextX] = false;
-                }
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		board = new int[N][M];
+		visited = new boolean[N][M];
 
-    static void bfs(int y, int x) {
-        int sum = arr[y][x];
-        int count = 0;
-        for (int i = 0; i < 4; i++) {
-            if (0 <= y + dy[i] && y + dy[i] < N && 0 <= x + dx[i] && x + dx[i] < M) {
-                sum += arr[y + dy[i]][x + dx[i]];
-                count++;
-            }
-        }
-        if (count == 3) {
-            if (result < sum) {
-                result = sum;
-            }
-        } else if (count == 4) {
-            for (int i = 0; i < 4; i++) {
-                int temp = sum - arr[y + dy[i]][x + dx[i]];
-                if (result < temp) {
-                    result = temp;
-                }
-            }
-        }
-    }
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				visited[i][j] = true;
+				dfs(i, j, board[i][j], 1);
+				visited[i][j] = false;
+
+				bfs(i, j);
+			}
+		}
+
+		System.out.println(answer);
+	}
+
+	static void dfs(int y, int x, int sum, int depth) {
+		if (depth >= 4) {
+			answer = Math.max(answer, sum);
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nextY = y + dy[i];
+			int nextX = x + dx[i];
+			if (isPossible(nextY, nextX)) {
+				visited[nextY][nextX] = true;
+				dfs(nextY, nextX, sum + board[nextY][nextX], depth + 1);
+				visited[nextY][nextX] = false;
+			}
+		}
+	}
+
+	static void bfs(int y, int x) {
+		int sum = board[y][x];
+		list.clear();
+
+		for (int i = 0; i < 4; i++) {
+			int nextY = y + dy[i];
+			int nextX = x + dx[i];
+			if (isPossible(nextY, nextX)) {
+				list.add(new Point(nextY, nextX));
+			}
+		}
+
+		if (list.size() == 3) {
+			for (Point point : list) {
+				sum += (board[point.y][point.x]);
+			}
+			answer = Math.max(answer, sum);
+		} else if (list.size() == 4) {
+			for (Point point : list) {
+				sum += (board[point.y][point.x]);
+			}
+
+			for (Point point : list) {
+				answer = Math.max(answer, sum - board[point.y][point.x]);
+			}
+		}
+	}
+
+	static boolean isPossible(int y, int x) {
+		return 0 <= y && y < N && 0 <= x && x < M && !visited[y][x];
+	}
 }
