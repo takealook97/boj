592bc193d508830a721b6ac77aff876c85bbd4ef
takealook97 <takealook97@naver.com>
2024-04-19 13:38:07 +0900

    [Silver II] Title: 랜선 자르기, Time: 124 ms, Memory: 14828 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,30 +1,43 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int K = sc.nextInt();
-        int N = sc.nextInt();
-        long[] arr = new long[K];
-        long max = 0;
-        for (int i = 0; i < K; i++) {
-            arr[i] = sc.nextLong();
-            max = Math.max(arr[i], max);
-        }
-        max++;
-        long min = 0;
-        long mid = 0;
-        while (min < max) {
-            long count = 0;
-            mid = (max + min) / 2;
-            for (int i = 0; i < K; i++) {
-                count += arr[i] / mid;
-            }
-            if (count < N) {
-                max = mid;
-            } else min = mid + 1;
-        }
-        System.out.println(min - 1);
-    }
+	static int K, N;
+	static long[] arr;
+	static long hi;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		K = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		arr = new long[K];
+		for (int i = 0; i < K; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			hi = Math.max(arr[i], hi);
+		}
+
+		hi++;
+		long lo = 0;
+		long mid = 0;
+		while (lo < hi) {
+			mid = lo + (hi - lo) / 2;
+
+			// set count
+			long count = 0;
+			for (int i = 0; i < K; i++) {
+				count += arr[i] / mid;
+			}
+
+			if (count < N) {
+				hi = mid;
+			} else {
+				lo = mid + 1;
+			}
+		}
+
+		System.out.println(lo - 1);
+	}
 }
