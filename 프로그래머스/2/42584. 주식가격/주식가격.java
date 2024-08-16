9d80e14e55e0e76ef6c5125a25d6ac15d051a504
takealook97 <takealook97@naver.com>
2024-08-16 19:19:21 +0900

    [level 2] Title: 주식가격, Time: 41.72 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int N = prices.length;
+        int[] answer = new int[N];
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+            stack.add(i);
+        }
+        
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = N - 1 - idx;
+        }
+        
+        return answer;
+    }
+}
